914f1c3a1c34319c652d70d3b3fd6f4cbd366ea1
abdmlkbd <abdmlkbd@gmail.com>
2025-07-25 10:47:08 +0600

    Learning For Loop concept

--- ForLoop.py
+++ ForLoop.py
@@ -4,7 +4,9 @@
 
 # Which means, there are 2 classrooms each having 5 students. 
 
-# For each class's 3rd student, I want to show its index no/position (in the list).
+# When the for loop gets Student No. 3 for each class, I want to print each to show:
+# Class 1's Student No. 3 
+# Class 2's Student No. 3 
 
 class_room = [1,2]
 studentRoll = [1,2,3,4,5]
@@ -12,7 +14,7 @@ studentRoll = [1,2,3,4,5]
 for i in range(len(class_room)):
     for j in range(len(studentRoll)):
         if j == 2: 
-            print(class_room[i])
-            print(studentRoll[j])
-
+            # print(class_room[i])
+            # print(studentRoll[j])
+            print("Class "+str(class_room[i])+"'s Student No." + str(studentRoll[j]))
             
